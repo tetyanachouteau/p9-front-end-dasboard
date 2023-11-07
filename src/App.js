2b6972b712b907f0a1e67cfb8574198372f70ae5
Tetyana <tetyana.chouteau@gmail.com>
2023-11-07 14:23:39 +0100

    Ajout des fichiers de base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+import Accueil from "./pages/accueil";
+import Dashboard from "./pages/dashboard";
+
+import Layout from './components/Layout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Routes>
+      <Route path="/" element={<Layout />} >
+        <Route index element={<Accueil />} />
+        <Route path="dashboard" element={<Dashboard />} />
+      </Route>
+      </Routes>
+      );
 }
 
 export default App;
+      
---
+++ src/components/Layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header';
+import { Outlet } from 'react-router-dom';
+import styles from './Layout.module.css';
+
+function Layout() {
+    return (
+        <div className={styles.layout}>
+            <Header />
+            <main>
+                <Outlet />
+            </main>
+        </div>
+    );
+}
+
+export default Layout;
