c33fb58f38623ef77615edb4768d025d4265483a
Tetyana <tetyana.chouteau@gmail.com>
2023-11-09 16:05:56 +0100

    Plassement des 6composents dans dashboard.jsx, css

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,28 +1,32 @@
-//import { useParams, Navigate } from "react-router-dom";
-//import data from '../data/data.js';
+// Dashboard.jsx
 import React from 'react';
-import MesuresDashboard from "../components/MesuresDashboard";
+import MesuresDashboard from '../components/MesuresDashboard';
 import HeaderDashboard from '../components/HeaderDashboard';
 import Poids1 from '../components/Poids1';
 import Radar2 from '../components/Radar2';
 import Objectifs3 from '../components/Objectifs3';
 import KPI4 from '../components/KPI4';
+import styles from './dashboard.module.css';
+
 function Dashboard() {
-    //let { id } = useParams()
-    //let datalist = data.find(el => el.id === id)
-    //let description= { description } 
     return (
-        <div>
-            <HeaderDashboard />
-            <Poids1/>
-            <Radar2/>
-            <Objectifs3/>
-            <KPI4/>
-            <MesuresDashboard/>
+        <div className={styles['dashboard-container']}>
+            <div className={styles['dashboard-top']}>
+                <HeaderDashboard />
+            </div>
+            <div className={styles['dashboard-middle']}>
+                <div className={styles['group-left']}>
+                    <Poids1 />
+                    <Objectifs3 />
+                    <Radar2 />
+                    <KPI4 />
+                </div>
+                <div className={styles['dashboard-right']}>
+                    <MesuresDashboard />
+                </div>
+            </div>
         </div>
     );
 }
 
 export default Dashboard;
-
-
