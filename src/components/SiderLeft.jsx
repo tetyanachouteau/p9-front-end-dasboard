e84a62758ead4ac15259eaa92ff43339235ba170
Tetyana <tetyana.chouteau@gmail.com>
2023-11-11 14:00:20 +0100

    Update SiderLeft (position)

--- src/components/SiderLeft.jsx
+++ src/components/SiderLeft.jsx
@@ -6,21 +6,23 @@ function SiderLeft() {
 
     return (
         <siderleft className={styles.siderleft}>
-            <div className={styles.icons}>
-                <Link to="/page1">
-                    <img src="/images/icon1.png" alt="Icon 1" />
-                </Link>
-                <Link to="/page2">
-                    <img src="/images/icon2.png" alt="Icon 2" />
-                </Link>
-                <Link to="/page3">
-                    <img src="/images/icon3.png" alt="Icon 3" />
-                </Link>
-                <Link to="/page4">
-                    <img src="/images/icon4.png" alt="Icon 4" />
-                </Link>
+            <div className={styles.iconsAndText}>
+                <div className={styles.icons}>
+                    <Link to="/page1">
+                        <img src="/images/icon1.png" alt="Icon 1" />
+                    </Link>
+                    <Link to="/page2">
+                        <img src="/images/icon2.png" alt="Icon 2" />
+                    </Link>
+                    <Link to="/page3">
+                        <img src="/images/icon3.png" alt="Icon 3" />
+                    </Link>
+                    <Link to="/page4">
+                        <img src="/images/icon4.png" alt="Icon 4" />
+                    </Link>
+                </div>
+                <div className={styles.text}>Copyright, SportSee 2020</div>
             </div>
-            <div className={styles.text}>Copyright, SportSee 2020</div>
         </siderleft >
     );
 }
