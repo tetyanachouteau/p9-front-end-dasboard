029705cac3d85f6f6ba0507b8762641e6ffb2f02
Tetyana <tetyana.chouteau@gmail.com>
2023-11-19 11:30:13 +0100

    Ajout du titre (triche) https://github.com/recharts/recharts/issues/478

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { BarChart, Bar, Label, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import { USER_ACTIVITY } from "../data/data"
 import { useParams } from 'react-router-dom';
 import styles from './Activity.module.css';
@@ -33,11 +33,11 @@ function Activity() {
     return (
         <ResponsiveContainer width="100%" height={300}>
             <BarChart data={activity.sessions}>
-                <Label value="Activité quotidienne" offset={0} position="top" />
+                <text x={0} y={20} fill="black" fontWeight="bold">Activité quotidienne</text>
                 <CartesianGrid vertical={false} strokeDasharray="3 3" />
                 <XAxis tickLine={false} />
                 <YAxis orientation="right" axisLine={false} tickLine={false} />
-                <Tooltip content={<CustomTooltip />} offset={60} position={{y:0}}/>
+                <Tooltip content={<CustomTooltip />} offset={60} position={{ y: 0 }} />
                 <Legend verticalAlign="top" align="right" iconType='circle' height={50} />
                 <Bar dataKey="kilogram" name="Poids (kg)" fill="#282D30" barSize={8} shape={<MaBar />} />
                 <Bar dataKey="calories" name="Calories brûlées (kCal)" fill="#E60000" barSize={8} shape={<MaBar />} />
