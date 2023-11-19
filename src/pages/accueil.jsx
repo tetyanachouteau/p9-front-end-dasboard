f8ae7f76ea7b6ff02dd5e7dee985534172db831a
Tetyana <tetyana.chouteau@gmail.com>
2023-11-19 08:40:55 +0100

    Ajout bouton profil

--- src/pages/accueil.jsx
+++ src/pages/accueil.jsx
@@ -11,15 +11,21 @@ function Accueil({ data }) {
         
         <div className={styles.profilContainer}>
             <h1>SportSee Start</h1>
-
-            <Link to={"/dashboard/"} className={styles.dashboardButton} >
+            <h2>Dashboard</h2>
+            <Link to={"/dashboard/12"} className={styles.dashboardButton} >
             Profil de Karl ID12 <h2>Toto</h2>
             </Link>
-
-            <Link to={"/dashboard/"} className={styles.profilContainer} >
+            <Link to={"/dashboard/18"} className={styles.dashboardButton} >
             <p>Profil de Césilia ID18 <h2>Titi</h2></p>
             </Link>
-            
+
+            <h2>Profil</h2>
+            <Link to={"/profile/12"} className={styles.dashboardButton} >
+            Profil de Karl ID12 <h2>Toto</h2>
+            </Link>
+            <Link to={"/profile/18"} className={styles.dashboardButton} >
+            <p>Profil de Césilia ID18 <h2>Titi</h2></p>
+            </Link>
         </div>
     );
 }
