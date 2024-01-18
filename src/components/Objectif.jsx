fd6bf3fe79774acbd89303fe837eac775c1b1e87
Tetyana <tetyana.chouteau@gmail.com>
2024-01-18 22:00:43 +0100

    Ajout de // aux composants

--- src/components/Objectif.jsx
+++ src/components/Objectif.jsx
@@ -1,36 +1,49 @@
-
 import React from 'react';
 import styles from "./Objectif.module.css"
 import { RadialBar, RadialBarChart, ResponsiveContainer } from 'recharts';
 
-function Objectif({score}) {
+function Objectif({ score }) {
+
+    // Définir les données pour le graphique radial
+
+    //Ces données sont ensuite utilisées dans le composant RadialBarChart pour 
+    //construire le graphique radial en fonction des valeurs spécifiées dans le tableau data. 
+
+    //Chaque élément du tableau représente une section du graphique, avec les propriétés suivantes :
+
+    //name: Le nom de la section, utilisé pour référencer la section dans le graphique.
+    //value: La valeur associée à la section. Dans le cas de la première section ("total"), 
+        //la valeur est le complément à 1 du score, et dans le cas de la deuxième section ("objectif"), la valeur est le score lui-même.
+    //fill: La couleur de remplissage de la section: couleur grise, pour la deuxième section: couleur rouge.
 
     const data = [
         {
-            name: "total",
-            value: 1 - score,
-            fill: "#FBFBFB"
+            name: "total",        // Nom de la première section du graphique
+            value: 1 - score,     // Valeur associée à la première section (complément à 1 du score)
+            fill: "#FBFBFB"       // Couleur de remplissage pour la première section
         },
         {
-            name: "objectif",
-            value: score,
-            fill: "#FF0000"
+            name: "objectif",     // Nom de la deuxième section du graphique
+            value: score,         // Valeur associée à la deuxième section (score)
+            fill: "#FF0000"       // Couleur de remplissage pour la deuxième section
         }
     ];
 
-    // https://recharts.org/en-US/api/RadialBarChart
-
+    // Configuration du composant Recharts RadialBarChart
+    // Plus d'informations: https://recharts.org/en-US/api/RadialBarChart
     return (
         <ResponsiveContainer width="100%" height={265} className={styles.responsive}>
             <RadialBarChart
-                innerRadius="80%"
-                barSize={10}
-                startAngle={90}
-                endAngle={450}
+                innerRadius="80%"   // Définir le rayon intérieur du graphique radial
+                barSize={10}        // Définir la taille des barres radiales
+                startAngle={90}     // Définir l'angle de départ du graphique
+                endAngle={450}      // Définir l'angle de fin du graphique
                 data={data}
             >
+                {/* Ajouter une étiquette pour le score */}
                 <text x={30} y={44} fill="black" fontWeight="bold">Score</text>
-                
+
+                {/* Configurer la barre radiale */}
                 <RadialBar
                     label={false}
                     background="#FFF"
@@ -38,13 +51,22 @@ function Objectif({score}) {
                     cornerRadius={5}
                     dataKey="value"
                 />
-                <text x="44%" y={112} fill="black" fontWeight="bold">{Math.floor(score*100)}%</text>
+
+                {/* Afficher le pourcentage du score */}
+                <text
+                    x="44%"
+                    y={112}
+                    fill="black"
+                    fontWeight="bold"
+                >
+                    {Math.floor(score * 100)}% {/* Afficher le pourcentage du score */}
+                </text>
+
+                {/* Ajouter des textes d'information */}
                 <text x="42%" y={132} fill="#AAA">de votre</text>
                 <text x="42%" y={152} fill="#AAA">objectif</text>
             </RadialBarChart>
-
         </ResponsiveContainer>
-
     );
 }
 
--- src/components/Radar.jsx
+++ src/components/Radar.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import styles from "./Radar.module.css"
 import { ResponsiveContainer, RadarChart, PolarGrid, PolarAngleAxis, Radar } from 'recharts';
@@ -7,6 +6,24 @@ function RadarComp({perf}) {
     // Extraire les données de performance spécifiques pour le graphique radar
     let data = perf.data.sort((a,b) => {return b.kind-a.kind});
 
+    //Dans cette partie du code, vous effectuez plusieurs opérations :
+
+//perf.data: Vous accédez à la propriété data de l'objet perf. 
+//Il est supposé que perf est un objet passé en tant que prop au composant RadarComp, 
+//et data contient les données de performance nécessaires pour le graphique radar.
+
+//.sort((a, b) => { return b.kind - a.kind }): tri les données par la propriété kind de manière décroissante. 
+//Cela signifie que les données seront triées en fonction de la valeur de kind de chaque élément, 
+//de la plus grande à la plus petite. 
+//Le tri est effectué en utilisant une fonction de comparaison ((a, b) => { return b.kind - a.kind }), 
+//où a et b sont deux éléments à comparer. Cette opération peut être utile si on 
+//affiche les catégories du graphique radar dans un ordre spécifique basé sur la valeur de kind.
+
+//En résumé, cette ligne de code extrait les données de performance spécifiques pour le graphique radar à partir de l'objet perf
+// et les trie en fonction de la propriété kind de manière décroissante. 
+//Cela peut être important pour garantir un ordre spécifique des catégories sur le graphique radar.
+
+    // Mapper les catégories pour les axes X avec des noms plus conviviaux
     let translate = {'cardio': 'Cardio',
     'energy': 'Energie',
     'endurance': 'Endurance',
@@ -20,34 +37,31 @@ function RadarComp({perf}) {
         return translate[perf.kind[data]];
     };
 
-
-    // https://recharts.org/en-US/api/RadarChart
-
-    // Composant RadarChart personnalisé
-return (
-    // Conteneur réactif pour le graphique (s'adapte à la largeur et à la hauteur de son parent)
-    <ResponsiveContainer width="100%" height={265} className={styles.responsive}>
-        {/* RadarChart avec Recharts */}
-        <RadarChart data={data} outerRadius="45%">
-            {/* Grille polaire pour le graphique radar */}
-            <PolarGrid />
-            {/* Axe des angles polaires (axe X) */}
-            <PolarAngleAxis
-                dataKey="kind"              // Clé de données pour les angles (les catégories à afficher sur l'axe X)
-                stroke='#FFF'               // Couleur de la ligne de l'axe des angles polaires en blanc
-                tickFormatter={customTickNameX}  // Fonction de formatage des étiquettes de l'axe X
-                tickLine={false}            // Désactivation des lignes de repère sur l'axe X
-            />
-            {/* Données radar à afficher sur le graphique */}
-            <Radar
-                dataKey="value"              // Clé de données pour les valeurs radar
-                fill="#FF0101"               // Couleur de remplissage du radar en rouge
-                fillOpacity={0.6}            // Opacité du remplissage du radar
-            />
-        </RadarChart>
-    </ResponsiveContainer>
-);
-
+    // Configuration du composant Recharts RadarChart
+    // Plus d'informations: https://recharts.org/en-US/api/RadarChart
+    return (
+        // Conteneur réactif pour le graphique (s'adapte à la largeur et à la hauteur de son parent)
+        <ResponsiveContainer width="100%" height={265} className={styles.responsive}>
+            {/* RadarChart avec Recharts */}
+            <RadarChart data={data} outerRadius="45%">
+                {/* Grille polaire pour le graphique radar */}
+                <PolarGrid />
+                {/* Axe des angles polaires (axe X) */}
+                <PolarAngleAxis
+                    dataKey="kind"              // Clé de données pour les angles (les catégories à afficher sur l'axe X)
+                    stroke='#FFF'               // Couleur de la ligne de l'axe des angles polaires en blanc
+                    tickFormatter={customTickNameX}  // Fonction de formatage des étiquettes de l'axe X
+                    tickLine={false}            // Désactivation des lignes de repère sur l'axe X
+                />
+                {/* Données radar à afficher sur le graphique */}
+                <Radar
+                    dataKey="value"              // Clé de données pour les valeurs radar
+                    fill="#FF0101"               // Couleur de remplissage du radar en rouge
+                    fillOpacity={0.6}            // Opacité du remplissage du radar
+                />
+            </RadarChart>
+        </ResponsiveContainer>
+    );
 }
 
 export default RadarComp;
